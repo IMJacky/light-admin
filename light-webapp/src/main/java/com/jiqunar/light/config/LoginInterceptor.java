aaf39e21b309a55839eabdae2a044e256bbc4915
wjg50058 <jieguang.wang@ly.com>
2020-06-04 13:18:39 +0800

    delete getExcludePathPatterns

--- light-webapp/src/main/java/com/jiqunar/light/config/LoginInterceptor.java
+++ light-webapp/src/main/java/com/jiqunar/light/config/LoginInterceptor.java
@@ -25,7 +25,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (customConfig.getAuthOpen() && !customConfig.getExcludePathPatterns().contains(request.getRequestURI())) {
+        if (customConfig.getAuthOpen()) {
             String token = request.getHeader("Access-Token");
             if (StringUtils.isBlank(token)) {
                 response.setContentType("application/json");
